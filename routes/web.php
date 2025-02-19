952d6c05034f1ed0b7b0089d6d6db765a696fae2
aqil <aqeelamaisa.am@gmail.com>
2025-02-19 10:36:48 +0700

    templating login and registrasi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[UserController::class,'login'])->name('login');
+Route::get('/register',[UserController::class,'register'])->name('register');
+Route::post('/register', [UserController::class, 'registerStore'])->name('register.store');
+Route::resource('users', UserController::class);
